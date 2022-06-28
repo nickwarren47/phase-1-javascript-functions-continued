9d1cbdab7576337322db52576d31ef1d1279bacc
Nick Warren <nickwarren47@gmail.com>
2022-06-28 17:34:24 -0600

    commit tree aec56998b569c690706c2c8314cac89b4d7a61e6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+// function saturdayFun(activity = "golfing", msg = "roller-skate"){
+//     return `${greeting}, ${msg}`;
+// }
+// saturdayFun();
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun('bathe my dog');
+
+function mondayWork(act = 'go to the office'){
+    return `This Monday, I will ${act}.`
+}
+function wrapAdjective (def = "*"){
+    return function innerFunction (insert = "special") {
+        return `You are ${def}${insert}${def}!`
+    }
+}
